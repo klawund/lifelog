f938807b5e180f5815f5552a5a4510db55defa28
klawund <klabundehiago@gmail.com>
2025-01-03 19:39:00 -0300

    initial commit

---
+++ src/test/java/com/klawund/lifelog/LifelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klawund.lifelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
